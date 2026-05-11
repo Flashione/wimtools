0d6707b871c8a34d064cccfb919d8b9ad12961c8
FlashiOne <43337858+Flashione@users.noreply.github.com>
2026-05-11 16:27:00 +0200

    Rename workflows to WimApply and WimCapture

--- WimTools/startup.cmd
+++ WimTools/startup.cmd
@@ -17,8 +17,8 @@ echo ==============================
 echo WimTools PE
 echo ==============================
 echo.
-echo 1 - WimForge  Apply Image
-echo 2 - WimVault  Capture Image
+echo 1 - WimApply    Apply Image
+echo 2 - WimCapture  Capture Image
 echo 3 - Command Prompt
 echo 4 - Reboot
 echo 5 - Shutdown
@@ -29,8 +29,8 @@ choice /c 12345 /m "Select option"
 if errorlevel 5 wpeutil shutdown
 if errorlevel 4 wpeutil reboot
 if errorlevel 3 cmd /k
-if errorlevel 2 call "%ROOT%\vault\capture.cmd"
-if errorlevel 1 call "%ROOT%\forge\apply.cmd"
+if errorlevel 2 call "%ROOT%\capture\capture.cmd"
+if errorlevel 1 call "%ROOT%\apply\apply.cmd"
 
 endlocal
 cmd /k
