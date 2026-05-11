b13f250acc7328c665e17e501b3a849efb7087d6
FlashiOne <43337858+Flashione@users.noreply.github.com>
2026-05-11 20:45:57 +0200

    Remove old root WimTools power helper

--- WimTools/common/set-power.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-powercfg /s SCHEME_MIN >nul 2>&1
-powercfg /change monitor-timeout-ac 0 >nul 2>&1
-powercfg /change standby-timeout-ac 0 >nul 2>&1
-powercfg /change hibernate-timeout-ac 0 >nul 2>&1
-
-exit /b 0
